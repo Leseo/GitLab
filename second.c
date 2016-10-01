48bd69c6ccc4b2fc8705928d64236ca9af965101
Leseo <apcatccnu@163.com>
2016-10-01 09:58:17 +0800

    A beautiful story

--- second.c
+++ second.c
@@ -3,5 +3,6 @@
 void main()
 {
     printf("This is a story.");
-    printf("A story has to be told.")
+    printf("A story has to be told.");
+    printf("A beautiful story has to be told again and again.")
 }
