e4490a5ef3c0175d4e1da186cf3a224dff383e3f
Leseo <apcatccnu@163.com>
2016-10-01 10:18:04 +0800

    a better story

--- second.c
+++ second.c
@@ -4,5 +4,6 @@ void main()
 {
     printf("This is a story.");
     printf("A story has to be told.");
-    printf("A beautiful story has to be told again and again.")
+    printf("A beautiful story has to be told again and again.");
+    printf("I can't come up with a better story.");
 }
